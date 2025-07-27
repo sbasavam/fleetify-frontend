1c51ad328db972efed766ee9594c102c378684a0
Sangana Basava M <sanganabasavam1999@gmail.com>
2025-07-27 14:23:20 +0530

    adedd required condition to date

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = 'http://localhost:5000/api';
 
 
-// const BASE_URL ='https://fleetify-backend.onrender.com/api'
+const BASE_URL ='https://fleetify-backend.onrender.com/api'
 
 
 const api = axios.create({
